510027af1650db396bb686115291c1f9055ae854
Miles Possing <7330275+milespossing@users.noreply.github.com>
2020-07-19 15:28:43 -0500

    Adding verbosity in error

--- scripts/add-post.js
+++ scripts/add-post.js
@@ -5,6 +5,10 @@ const fs = require('fs');
 
 var client = new MongoClient(new Server('localhost',27017));
 client.connect(function (mongoError, mongoClient) {
+  if (mongoError) {
+    console.log(mongoError)
+    return;
+  }
   var post = JSON.parse(fs.readFileSync(argv.path));
   let collection = mongoClient.db('blog').collection('posts');
   collection.insertOne(post);
