3968724f1ad6d4c4c4d3af6485cc5db7833a92ef
Miles Possing <7330275+milespossing@users.noreply.github.com>
2020-07-19 15:10:23 -0500

    Adding add-post script

---
+++ scripts/add-post.js
@@ -0,0 +1,12 @@
+const {argv} = require('yargs');
+const {seedFile} = require('./seed-database');
+const {MongoClient, Server} = require('mongodb');
+const fs = require('fs');
+
+var client = new MongoClient(new Server('localhost',27017));
+client.connect(function (mongoError, mongoClient) {
+  var post = JSON.parse(fs.readFileSync(argv.path));
+  let collection = mongoClient.db('blog').collection('posts');
+  collection.insertOne(post);
+  console.log('finished');
+});
